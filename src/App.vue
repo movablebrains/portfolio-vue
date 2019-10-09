fac2db97a54421ad5f10159e8c2eb93a247dc739
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-09 17:34:37 +0800

    Code cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,18 @@
   <div id="app">
     <Navigation />
     <Header />
-    <!-- <div>{{ myJson }}</div> -->
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
 import Navigation from './components/Navigation.vue'
 import Header from './components/Header.vue'
-
-// import json from './data/navigation.json'
 
 export default {
   name: 'app',
   data() {
     return {
-      // myJson: json
     }
   },
   components: {
