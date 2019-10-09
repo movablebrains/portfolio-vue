0c9f33bc11bc88d866a7ae553af16a4cbe134d6d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-09 14:38:06 +0800

    Added imports from App.vue, Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+import './assets/scss/style.scss';
+
 Vue.config.productionTip = false
 
 new Vue({
