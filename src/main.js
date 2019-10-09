02b3fec5394fd8afe647dbd962415009ef70c108
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-09 17:36:59 +0800

    Fixed issue with Font Awesome not outputting

--- src/main.js
+++ src/main.js
@@ -8,11 +8,22 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+
+dom.watch()
 
 library.add(fas, fab)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 import './assets/scss/style.scss';
+
+import '../node_modules/jquery/dist/jquery'
+
+import '../node_modules/popper.js/dist/umd/popper'
+
+import '../node_modules/stream/assets/vendors/bootstrap/js/bootstrap'
+
+// import '../node_modules/stream/assets/js/global';
 
 Vue.config.productionTip = false
 
