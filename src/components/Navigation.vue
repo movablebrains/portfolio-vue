f0a4f88a3ef8fe257b4c01d32033fba743a93b17
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-09 17:36:08 +0800

    Markup cleanup

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,38 +1,37 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="info" class="js-navbar-scroll navbar fixed-top navbar-expand-lg navbar-dark">
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item v-for="navigation in navigationList" :key="navigation.title" :href="navigation.href">{{ navigation.title }}</b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
+    <b-navbar toggleable="lg" type="dark" class="js-navbar-scroll navbar fixed-top navbar-expand-lg navbar-dark">
+        <b-container>
+            <b-navbar-brand href="/"><img :src="`${publicPath}images/logo.svg`" alt="Rotsen Mark Acob" width="50"></b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto mt-2 mt-lg-0">
+                    <b-nav-item class="mr-4 mb-2 mb-lg-0" v-for="navigation in navigationList" :key="navigation.title" :href="navigation.href">{{ navigation.title }}</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-container>
     </b-navbar>
 </template>
 
 <script>
-    import axios from 'axios';
+    // import axios from 'axios';
     import json from '../data/navigation.json';
     export default {
         name: "Navigation",
         data() {
             return {
-                navigationList: json
+                navigationList: json,
+                baseUrl: process.env.VUE_APP_BASE_URL,
+                publicPath: process.env.BASE_URL
             }
         },
         mounted: function() {
-            // console.log('Hello World');
             // this.getNavigation();
-            // axios.get(json)
-            //     .then(response => (this.navigationList = response.data))
         },
         methods: {
-            getNavigation: function() {
-                // fetch('test.json')
-                //     .then(response => response.json())
-                //     .then(data => (this.navigationList = data));
-                axios.get(json)
-                    .then(response => (this.navigationList = response.data));
-            }
+            // getNavigation: function() {
+            //     axios.get(this.baseUrl + 'data/navigation.json')
+            //         .then(response => (this.navigationList = response.data));
+            // }
         }
     }
 </script>
