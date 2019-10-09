01d25e0d88b3b6d1535c7806ce9147241f55578f
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-09 14:37:38 +0800

    Moved imports to main.js

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,10 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
-import './assets/scss/style.scss';
 import Navigation from './components/Navigation.vue'
 import Header from './components/Header.vue'
 
-Vue.use(BootstrapVue)
 // import json from './data/navigation.json'
 
 export default {
