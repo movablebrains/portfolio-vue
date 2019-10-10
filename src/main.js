041bed299eeb4aebd9d1b6ef24ae3f272249236d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-10 12:58:30 +0800

    Added Vue Meta

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueMeta from 'vue-meta'
+Vue.use(VueMeta, {
+  refreshOnceNavigation: true
+})
+
 import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
