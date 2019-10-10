a50a3f0ec8eceadcfa5e83874190a1f81122e631
webdevsuperfast <acob.rotsen@hotmail.com>
2019-10-10 08:19:03 +0800

    Added additional components

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import json from '../data/portfolio.json'
+
+export default {
+    name: "Portfolio",
+    data() {
+        return {
+            portfolioList: json
+        }
+    },
+    mounted: function() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import json from '../data/skills.json'
+
+export default {
+    name: "Skills",
+    data() {
+        return {
+            skillsList: json
+        }
+    },
+    mounted: function() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
