8eaa1e365dbe96911529d79f02dcb4b53559b4ba
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-11 08:37:07 +0800

    Added lazyload markup

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -9,7 +9,7 @@
                 <b-col lg="6" :class="`mb-5 mb-lg-7 px-lg-5`" v-for="(testimonial, index) in testimonialList" :key="index">
                     <blockquote class="u-blockquote-v2 rounded mb-5">{{ testimonial.review }}</blockquote>
                     <div class="media u-font-size-90">
-                        <img class="d-flex align-self-center rounded-circle u-blockquote-v2__image u-box-shadow-lg mx-3 mt-2 lazyload" :src="`${publicPath}images/upwork.png`" :alt="`${ testimonial.name }`">
+                        <img class="lazyload d-flex align-self-center rounded-circle u-blockquote-v2__image u-box-shadow-lg mx-3 mt-2 lazyload" :data-src="`${publicPath}images/upwork.png`" :alt="`${ testimonial.name }`">
                         <div class="media-body align-self-center">
                             <strong class="d-block">{{ testimonial.name }}</strong>
                             <span class="u-font-size-75 text-dark" v-if="testimonial.location != ''">{{ testimonial.location }}</span>
