345b8cb49efa75ef2364a8ffd8f099c6ea279c12
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-13 20:00:17 +0800

    Only call needed icons

--- src/main.js
+++ src/main.js
@@ -23,17 +23,16 @@ import BootstrapVue from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faWordpress, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 
 dom.watch()
 
-library.add(fas, fab)
+library.add(faGithub, faWordpress, faLinkedin, faEnvelope)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-// import '../node_modules/jquery/dist/jquery'
 import '../jquery-global'
 import '../node_modules/popper.js/dist/umd/popper'
 import '../node_modules/stream/assets/vendors/bootstrap/js/bootstrap'
