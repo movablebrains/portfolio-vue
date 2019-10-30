8c59232f3302e9f236b0b92f49304ecb20ccf495
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-30 11:32:31 +0800

    Added views

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <main id="content">
+    <Header />
+    <About />
+    <Border />
+    <Experience />
+    <Skills />
+    <Portfolio />
+    <Testimonial />
+    <Contact />
+  </main>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import About from '@/components/About.vue'
+import Experience from '@/components/Experience.vue'
+import Skills from '@/components/Skills.vue'
+import Portfolio from '@/components/Portfolio.vue'
+import Testimonial from '@/components/Testimonial.vue'
+import Contact from '@/components/Contact.vue'
+import Border from '@/components/Border.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Header,
+    About,
+    Experience,
+    Portfolio,
+    Skills,
+    Testimonial,
+    Contact,
+    Border
+  }
+}
+</script>
