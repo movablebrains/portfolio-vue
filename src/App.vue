735c8ce67c2e86b00fa06c32cf422a2c5e9b7dcf
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-30 11:32:52 +0800

    Moved to views

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
     <Navigation />
-    <Header />
-    <About />
-    <Border />
-    <Experience />
-    <Skills />
-    <Portfolio />
-    <Testimonial />
-    <Contact />
+    <router-view />
     <Footer />
   </div>
 </template>
@@ -16,15 +9,7 @@
 <script>
 
 import Navigation from './components/Navigation.vue'
-import Header from './components/Header.vue'
-import About from './components/About.vue'
-import Experience from './components/Experience.vue'
-import Skills from './components/Skills.vue'
-import Portfolio from './components/Portfolio.vue'
-import Testimonial from './components/Testimonial.vue'
 import Footer from './components/Footer.vue'
-import Contact from './components/Contact.vue'
-import Border from './components/Border.vue'
 
 import 'lazysizes'
 
@@ -40,15 +25,7 @@ export default {
   },
   components: {
     Navigation,
-    Header,
-    About,
-    Experience,
-    Portfolio,
-    Skills,
-    Testimonial,
-    Footer,
-    Contact,
-    Border
+    Footer
   }
 }
 </script>
