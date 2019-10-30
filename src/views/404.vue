a3a9c9f0dfaf63729db569d14c2ab98b6b683c77
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2019-10-30 11:32:31 +0800

    Added views

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="content">
+    <NotFound />
+  </main>
+</template>
+
+<script>
+import NotFound from '@/components/404.vue'
+
+export default {
+  name: '404',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    NotFound
+  }
+}
+</script>
